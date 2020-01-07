97e0d1293023bf4eacf1d10b7b0edf32accd9e66
Adam Kwiatek <kwiatek.adam@poczta.fm>
2020-01-07 02:29:23 +0100

    install.sh

--- install.sh
+++ install.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env sh
 
-chkPython2()
+chkPython3()
 {
     cmd=$1
     ret=$($cmd -V 2>&1)
     case "$ret" in
-    "Python 2."*)
+    "Python 3."*)
         return 0
         ;;
     *)
@@ -14,21 +14,21 @@ chkPython2()
     esac
 }
 
-findPython2()
+findPython3()
 {
-    cmd_list="python python2 python27 python2.7 python26 python2.6"
+    cmd_list="python python3 python38 python3.8 python37 python3.7 python36 python3.6"
     for cmd in $cmd_list; do
-        if chkPython2 $cmd; then
+        if chkPython3 $cmd; then
             found_python=$cmd
             break
         fi
     done
 
     if [ "$found_python" = "" ]; then
-        echo "cannot find python2 automatically" >&2
+        echo "cannot find python3 automatically" >&2
         while true; do
-            read -p "please input your python 2 command: " cmd
-            if chkPython2 "$cmd"; then
+            read -p "please input your python 3 command: " cmd
+            if chkPython3 "$cmd"; then
                 found_python=$cmd
                 break
             fi
@@ -39,9 +39,9 @@ findPython2()
     echo $found_python
 }
 
-python=$(findPython2)
-echo "find python2 -> $python"
+python=$(findPython3)
+echo "find python3 -> $python"
 
 cd autoload
 $python setup.py build
-cp build/lib*/fuzzycomt.so .
+cp build/lib*/fuzzycomt*.so ./fuzzycomt.so
